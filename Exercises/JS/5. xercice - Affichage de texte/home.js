b91576004aae713de96ecdcc20fc48574a9ee202
Tarik Asri <a.tarik92@gmail.com>
2020-12-09 13:46:41 +0100

    correction de suivi ncode

--- Exercises/JS/5. xercice - Affichage de texte/home.js
+++ Exercises/JS/5. xercice - Affichage de texte/home.js
@@ -6,4 +6,4 @@ var Prenom = window.prompt("Invite de script:\nEntrez votre prénom");
 if (window.confirm("Etes-vous un homme?") == true, pre = "Mr.");
 //else (window.confirm("Etes-vous une femme?") == true, pre = "Mrs");
 //comment fair pour afficher l'option "femme"
-alert("Hello " + pre + " " + Nom + " " + Prenom + ",\nBienvenue sur notre site web!");
+alert("Hello " + pre + " " + Nom + " " + Prenom + ",\nBienvenue sur notre site web!"); 
