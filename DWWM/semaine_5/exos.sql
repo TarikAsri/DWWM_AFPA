1d4a44b99f3f0d256a2954e03c3a958a961589de
Tarik Asri <a.tarik92@gmail.com>
2021-02-22 10:52:18 +0100

    exos triggers

--- DWWM/semaine_5/exos.sql
+++ DWWM/semaine_5/exos.sql
@@ -72,7 +72,91 @@ BEGIN
     FROM suppliers
     join products
     on sup_id = pro_sup_id;
-END
+END |
 DELIMITER ;
 
 -- EXO 1.2
+
+
+
+-- - - - - - - - - - - - - - -
+-- E X O s - T R I G G E R S -
+-- - - - - - - - - - - - - - -
+
+-- Travail la realiser
+-- 1. Mettez en place ce trigger, puis ajoutez un produit dans une commande, vérifiez que le champ total est bien mis à jour.
+
+DELIMITER |
+CREATE TRIGGER maj_total_insert
+AFTER INSERT ON lignedecommande
+FOR EACH ROW
+BEGIN
+    DECLARE id_c INT;
+    DECLARE tot DOUBLE;
+    SET id_c = NEW.id_commande; -- nous captons le numéro de commande concerné
+    SET tot = (SELECT sum(prix*quantite) FROM lignedecommande WHERE id_commande=id_c); -- on recalcul le total
+    UPDATE commande SET total=tot WHERE id=id_c; -- on stock le total dans la table commande
+END |
+DELIMITER;
+
+INSERT INTO lignedecommande (id_commande, id_produit, quantite, prix) VALUES
+(4, 5, 5, 700);
+-- Works!
+
+-- 2. Ce trigger ne fonctionne que lorsque l'on ajoute des produits, les modifications ou suppressions ne permettent pas de recalculer le total. Comment pourrait-on faire ?
+DELIMITER |
+CREATE TRIGGER maj_total_update
+AFTER UPDATE ON lignedecommande
+FOR EACH ROW
+BEGIN
+    DECLARE id_c INT;
+    DECLARE tot DOUBLE;
+    SET id_c = NEW/OLD.id_commande; -- NEW or OLD? I'm really not sure
+    SET tot = (SELECT sum(prix*quantite) FROM lignedecommande WHERE id_commande=id_c); -- on recalcul le total
+    UPDATE commande SET total=tot WHERE id=id_c; -- on stock le total dans la table commande
+END |
+DELIMITER;
+
+UPDATE lignedecommande 
+SET 
+id_commande = 4,
+id_produit = 5,
+quantite = 20,
+prix = 2
+WHERE lignedecommande.quantite = 10;
+-- works!
+-----------------------------------------
+DELIMITER |
+CREATE TRIGGER maj_total_delete
+AFTER DELETE ON lignedecommande
+FOR EACH ROW
+BEGIN
+    DECLARE id_c INT;
+    DECLARE tot DOUBLE;
+    SET id_c = OLD.id_commande; -- nous captons le numéro de commande concerné
+    SET tot = (SELECT sum(prix*quantite) FROM lignedecommande WHERE id_commande=id_c); -- on recalcul le total
+    UPDATE commande SET total=tot WHERE id=id_c; -- on stock le total dans la table commande
+END |
+DELIMITER;
+
+DELETE FROM lignedecommande
+WHERE quantite = 20
+AND id_produit = 5;
+-- works!
+---------------------------------------
+
+-- 3. Un champ remise était prévu dans la table commande. Comment pourrait-on le prendre en compte ? (Only INSERT)
+
+DELIMITER |
+CREATE TRIGGER maj_total 
+AFTER INSERT ON lignedecommande 
+FOR EACH ROW
+BEGIN
+    DECLARE id_c INT;
+    DECLARE tot DOUBLE;
+    SET id_c = NEW.id_commande; -- nous captons le numéro de commande concerné
+    SET tot = (SELECT sum(prix*quantite) FROM lignedecommande WHERE id_commande=id_c); -- on recalcul le total
+    UPDATE commande SET total=tot WHERE id=id_c; -- on stock le total dans la table commande
+    UPDATE commande SET remise = (SELECT (total/100)*5);
+END |
+DELIMITER;
